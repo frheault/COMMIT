7e2add2e748412d6279681dcc5f2f75555424645
frheault <francois.m.rheault@usherbrooke>
2020-05-08 12:21:18 -0400

    Switch to install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import numpy
-import amico
-
-amico_version = amico.__version__.split('.')
-amico_version = [int(version_val) for version_val in amico_version]
-if amico_version[0] == 1 and amico_version[1] < 2:
-    raise RuntimeError( 'COMMIT requires AMICO v1.2.0 or above. Current AMICO version is %s' % amico.__version__ )
 
 # Cython extension to create the sparse data structure from a tractogram
 # for the computation of matrix-vector multiplications
@@ -48,6 +42,7 @@ setup(
     cmdclass = {'build_ext':build_ext},
     ext_modules = [ ext1, ext2, ext3 ],
     packages=['commit','commit.operator'],
+    install_requires=['dipy', 'cython', 'amico'],
     package_data={
         'commit.operator':["*.*"], # needed by pyximport to compile at runtime
     },
